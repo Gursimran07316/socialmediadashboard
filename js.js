6409a24067ba5e1645cc30967e26315e5db70cbd
Gursimran <60434846+Gursimran07316@users.noreply.github.com>
2020-04-15 10:33:02 +0530

    update functionality

--- js.js
+++ js.js
@@ -2,8 +2,8 @@ const wrapper = document.querySelector('.wrapper');
 const grid_1 = document.querySelector('.grid-1');
 const grid_2 = document.querySelector('.grid-2');
 const header = document.querySelector('header');
-document.querySelector('.switch').addEventListener('click', (e) => {
-    e.target.parentNode.classList.toggle('active');
+document.querySelector('.switchHolder').addEventListener('click', (e) => {
+    e.target.classList.toggle('active');
     wrapper.classList.toggle('active');
     grid_1.classList.toggle('active');
     grid_2.classList.toggle('active');
